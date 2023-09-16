404223ee44eb55bd0df94a995b505ac71930b6eb
4KRO5 <4kro54k@gmail.com>
2023-09-15 17:47:52 -0700

    Implementación Login

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Aquí puedes implementar la lógica de autenticación
+    // Comprueba si el nombre de usuario y la contraseña son válidos
+    // y realiza las acciones necesarias
+    if (username === "usuario" && password === "contraseña") {
+      // Autenticación exitosa, puedes redirigir al usuario a la siguiente pantalla
+      alert("Inicio de sesión exitoso");
+    } else {
+      // Autenticación fallida, muestra un mensaje de error
+      alert("Nombre de usuario o contraseña incorrectos");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: 'https://picsum.photos/200/200' }}
+        style={styles.image}
+      />
+      <Text style={styles.appName}>TaskMaster</Text>
+      <Text style={styles.loginText}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre de usuario o correo electrónico"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.forgotPassword}>
+        <Text>Forgot Password?</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +52,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff'
   },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100
+  },
+  appName: {
+    fontSize: 24,
+    marginVertical: 10
+  },
+  loginText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  },
+  loginButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    width: 300,
+    alignItems: 'center'
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  forgotPassword: {
+    marginVertical: 10
+  }
 });
+
+export default App;
