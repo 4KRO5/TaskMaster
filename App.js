1062e48356c1e635b143da4959d843fc03d2c481
4KRO5 <4kro54k@gmail.com>
2023-09-22 10:16:30 -0700

    Implementación de navegación

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
 import ForgotPassword from "./Components/ForgotPassword";
 import ResetPassword from "./Components/ResetPassword";
 
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <View>
-      <Login />
-      {/* <SignUp />
-      <ForgotPassword />
-      <ResetPassword /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="ResetPassword" component={ResetPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- Components/Login.js
+++ Components/Login.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
-import * as Animatable from 'react-native-animatable'; // Importa Animatable
+import { useNavigation } from "@react-navigation/native";
+import * as Animatable from 'react-native-animatable';
 import { styles } from "./Style/Style";
 
 export default function Login() {
+  const navigation = useNavigation();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(""); // Nuevo estado para el mensaje de error
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleLogin = () => {
     if (username === "usuario" && password === "contraseña") {
       alert("Inicio de sesión exitoso");
     } else {
-      setErrorMessage("Credenciales no válidas. Intente de nuevo."); // Establece el mensaje de error
+      setErrorMessage("Credenciales no válidas. Intente de nuevo.");
     }
+  };
+
+  const handleSignUpRedirect = () => {
+    navigation.navigate("SignUp");
+  };
+
+  const handleForgotPasswordRedirect = () => {
+    navigation.navigate("ForgotPassword"); // Asegúrate de que "ForgotPassword" sea el nombre correcto
   };
 
   return (
@@ -42,14 +52,17 @@ export default function Login() {
       </TouchableOpacity>
       {errorMessage ? (
         <Animatable.View
-          animation="slideInLeft" // Puedes ajustar la animación según tus preferencias
+          animation="slideInLeft"
           style={styles.errorContainer}
         >
           <Text style={styles.errorMessage}>{errorMessage}</Text>
         </Animatable.View>
       ) : null}
-      <TouchableOpacity style={styles.forgotPassword}>
+      <TouchableOpacity style={styles.forgotPassword} onPress={handleForgotPasswordRedirect}>
         <Text>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginRedirect} onPress={handleSignUpRedirect}>
+        <Text>Don't have an account? Sign Up</Text>
       </TouchableOpacity>
     </View>
   );
